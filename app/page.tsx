6ee734c3348c5ed85720e483972ab4649f5e72ff
liangtingtsai <coltaim530@gmail.com>
2025-07-29 20:38:18 -0600

    rename the skeleton file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import CubeComponent from "./3d-cube";
+import CubeComponent from "./3d-cube.tsx";
 
 
 export default function Home() {
